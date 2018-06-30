a27fe9c80d49f3043d08256d383338780c55d31c
vicentedpsantos <vicentedpsantos@gmail.com>
2018-06-29 23:23:24 -0300

    first commit

---
+++ entertainment_center.py
@@ -0,0 +1,149 @@
+# This Python file uses the following encoding: utf-8
+import media
+import fresh_tomatoes
+
+the_lure = media.Movie("The Lure",
+                       "familiar drama about a young couple torn between individual dreams and professional desires",
+                       "http://img.goldposter.com/2016/05/The-Lure_poster_goldposter_com_1.jpg?x-oss-process=image/resize,m_fill,h_800,w_535/quality,q_80",
+                       "https://www.youtube.com/watch?v=WgWe9QVqrl0")
+
+icaros_a_vision = media.Movie("Icaros, a vision",
+                              "A journey into the deep, dark regions of the Amazonian wild",
+                              "https://m.media-amazon.com/images/M/MV5BMmMxYjk5MzItNzA5NC00MzVhLWIxMDAtNTg0YWMyNmM3Y2Q0XkEyXkFqcGdeQXVyNjU0ODQxODk@._V1_UY1200_CR109,0,630,1200_AL_.jpg",
+                              "https://www.youtube.com/watch?v=54fv-tP7cDg")
+
+thelma = media.Movie("Thelma",
+                     "A girl with unholy powers—albeit one that still plays in a decidedly Trier-ian register",
+                     "http://img.goldposter.com/2017/09/thelma_poster_goldposter_com_4.jpg?x-oss-process=image/resize,m_fill,h_800,w_600/quality,q_80",
+                     "https://www.youtube.com/watch?v=vgQMHG9SGlU")
+
+city_of_ghosts = media.Movie("City of Ghosts",
+                             "ISIS has claimed the Syrian city of Raqqa as the capital of its so-called Caliphate—and, at the same time, been opposed by a band of local \"citizen journalists\"",
+                             "https://cdn1.theyoungfolks.com/wp-content/uploads/2017/04/27015809/cityofghosts-770x770.jpg",
+                             "https://www.youtube.com/watch?v=jBeqGcMaC-A")
+
+alien_covenant = media.Movie("Alien: Covenant",
+                              "A familiar battle between human colonists and angry xenomorphs, after the former decide to investigate a mysterious distress signal from a nearby planet.",
+                              "https://www.otakupt.com/wp-content/uploads/2017/03/Covenast.jpg",
+                              "https://www.youtube.com/watch?v=svnAD0TApb8")
+
+super_dark_times = media.Movie("Super Dark Times",
+                               "Horrifying truths about teen boys’ capacity for chaos.",
+                               "https://3.bp.blogspot.com/-3WVzL1R_WrU/WmyHlpt1mAI/AAAAAAAABGY/EdoVEkyTesYX_DOrOVLFjerGLr1hqxnQACLcBGAs/s1600/SUPER_DARK_TIMES_POSTER.jpg",
+                               "https://www.youtube.com/watch?v=qG0af8nliCE")
+
+the_square = media.Movie("The Square",
+                         "Museum Director inds himself at the center of a personal maelstrom thanks to, among other things, a marketing campaign.",
+                         "https://m.media-amazon.com/images/M/MV5BODljYTUxYzktNzlkYS00OTc1LWE5ZWEtZTgwNTY0MzdlNzM4XkEyXkFqcGdeQXVyNTM2NTg3Nzg@._V1_SY1000_CR0,0,712,1000_AL_.jpg",
+                         "https://www.youtube.com/watch?v=EUzRjRv0Ib0")
+
+atomic_blonde = media.Movie("Atomic Blonde",
+                            "Theron's MI6 spy navigating a sea of Berlin duplicity on a mission to learn why her espionage cohort-lover was murdered.",
+                            "https://3.bp.blogspot.com/-AI6ggzIuLaY/WMQdL5sJXNI/AAAAAAAA10A/jLNNT1E_RacEDR4Fdgluctj4d80jSRVewCLcB/s1600/atomic-blonde-poster.jpg",
+                            "https://www.youtube.com/watch?v=d1mlDaiFx-s")
+
+lady_bird = media.Movie("Lady Bird",
+                        "A Catholic high school senior who longs to escape her stultifying lower-middle-class Sacramento environs for East Coast college life.",
+                        "https://http2.mlstatic.com/postercartaz-filme-lady-bird-hora-de-voar-D_NQ_NP_906370-MLB26867443950_022018-F.jpg",
+                        "https://www.youtube.com/watch?v=cNi_HC839Wo")
+
+lucky = media.Movie("Lucky",
+                    "Smoking like a chimney and dispensing wisdom with off-hand profundity, Stanton’s Lucky is an aged loner living in the middle of the desert, his life spent puffing on cancer sticks, exercising, watching game shows, and hanging out at the local bar with, among other regulars.",
+                    "http://www.vortexcultural.com.br/images/2018/04/Lucky-poster.jpg",
+                    "https://www.youtube.com/watch?v=2KLLkj84GAo",)
+                    
+good_time = media.Movie("Good Time",
+                        "In this breakneck nocturnal thriller, Pattinson is Connie, a low-level hood who finds himself on a desperate search for bailout cash after a bank robbery goes awry and his accomplice—his mentally challenged brother Nick —is arrested and given a one-way ticket to Rikers Island.",
+                        "https://i.pinimg.com/originals/bc/f1/f8/bcf1f8e3c700bf311e29641d7171721e.jpg",
+                        "https://www.youtube.com/watch?v=nrR-SbCRgCU")
+
+the_killing_of_a_sacred_deer = media.Movie("The Killing of a Sacred Deer",
+                                           "A pitch-black comedy of misery whose laughs spring forth from the mounting insanity of its story",
+                                           "https://3.bp.blogspot.com/-sI3-9p6ix-I/WZoMMol4M6I/AAAAAAAA2T0/v95h6OwiZ2c9_dNIarFORn4q6CrI0DBgACLcBGAs/s1600/killing-sacred-deer.jpg",
+                                           "https://www.youtube.com/watch?v=CQFdGfwChtw")
+
+dawson_city = media.Movie("Dawson City: Frozen Time",
+                          "Hundreds of reels of silent movies—those that were buried in a swimming pool, rather than just tossed into the Yukon River—were unearthed in 1978 in the northern Canadian town of Dawson City.",
+                          "http://br.web.img3.acsta.net/r_1280_720/pictures/17/01/31/15/39/015417.jpg",
+                          "https://www.youtube.com/watch?v=oEbHM8Vsvlo")
+
+the_blackcoats_daughter = media.Movie("The Blackcoat's Daughter",
+                                      "In upstate New York, Kat is left by her parents to spend winter break at her boarding school alongside more popular Rose.",
+                                      "https://m.media-amazon.com/images/M/MV5BMTY2NTQ3NTEzNF5BMl5BanBnXkFtZTgwMzA0MzY1OTE@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+                                      "https://www.youtube.com/watch?v=pRc_-iK3RVE")
+
+i_called_him_morgan = media.Movie("I Called Him Morgan",
+                                  "Lee Morgan was one of the mid-century jazz scene's brightest lights, until his life was cut tragically short when his wife Helen fatally gunned him down in a New York.",
+                                  "http://br.web.img3.acsta.net/r_1280_720/pictures/17/01/10/19/12/435203.jpg",
+                                  "https://www.youtube.com/watch?v=yxLByThNvWU")
+
+the_lost_city_of_z = media.Movie("The Lost City of Z",
+                                 "Early 20th century British explorer sets out to locate a lost South American civilization that he believed to be more advanced than any previously discovered.",
+                                 "http://vertentesdocinema.com/wp-content/uploads/2017/06/the-lost-city-of-z-poster.jpg",
+                                 "https://www.youtube.com/watch?v=wwjtdOqTmrA")
+
+phantom_thread = media.Movie("Phantom Thread",
+                             "An enthralling, rapturous psychodrama set in the world of 1950s London high fashion, the duo’s latest collaboration stars Day-Lewis as Reynolds Woodcock, a celebrated dressmaker who lives by a prim-and-proper daily (and artistic) routine.",
+                             "https://i.pinimg.com/originals/63/02/2f/63022ff9113f3acbc8a1c11d7a1a9a12.jpg",
+                             "https://www.youtube.com/watch?v=xNsiQMeSvMk")
+
+i_dont_feel_at_home = media.Movie("I Don't Feel At Home",
+                                  "After her house is burglarized, nurse Ruth partners with her rat-tailed martial-arts-loving neighbor Tony to recover her stolen belongings",
+                                  "https://3.bp.blogspot.com/-m52RBWWNe0o/WWV1UgGPJgI/AAAAAAAACaE/RatEwwzhyuMfwO1CyJjFGqh5CZIg5yovwCLcBGAs/s1600/i-dont-feel-at-home-in-this-world-anymore-590e339849de9.jpg",
+                                  "https://www.youtube.com/watch?v=a891D5_bGY4")
+
+columbus = media.Movie("Columbus",
+                       "As strikingly unique as the Indiana buildings its characters visit, Columbus is a boy-meets-girl tale that cares less for romance than for the unlikely, intrinsic ties that bind seemingly disparate souls.",
+                       "http://br.web.img3.acsta.net/r_1280_720/pictures/17/08/17/13/26/546003.jpg",
+                       "https://www.youtube.com/watch?v=r3dcnV6Z9Zs")
+
+marjorie_prime = media.Movie("Marjorie Prime",
+                             "At a seaside home, Marjorie spends her final days conversing with a holographic projection that resembles her late husband Walter",
+                             "http://2.bp.blogspot.com/-bp_ArfB8fxI/Vngu5ZJsyyI/AAAAAAAAKpE/W3BUYpViPQs/s1600/marjorie%2Bprime359.jpg",
+                             "https://www.youtube.com/watch?v=SccmZSOW3OA")
+
+dunkirk = media.Movie("Dunkirk",
+                      "A dramatic account of the WWII evacuation of Dunkirk, France's beaches in 1941",
+                      "https://i.pinimg.com/originals/dc/5c/85/dc5c85ee3569a61eda6791e5ce658c8c.png",
+                      "https://www.youtube.com/watch?v=F-eMt3SrfFU")
+
+lady_macbeth = media.Movie("Lady Macbeth",
+                           "A young woman is sold into marriage to an older landowner, whose nastiness is only surpassed by that of his domineering father.",
+                           "http://br.web.img3.acsta.net/r_1280_720/pictures/17/03/20/10/26/119027.jpg",
+                           "https://www.youtube.com/watch?v=2Z0N8ULhuUA")
+
+wormwood = media.Movie("Wormwood",
+                       "A government biochemist whose mysterious 1953 death out a New York City hotel room window was first deemed a suicide, then the byproduct of a CIA mind-control program, and then something more sinister still.",
+                       "https://ewedit.files.wordpress.com/2017/12/wormwood.jpg",
+                       "https://www.youtube.com/watch?v=b01DL8DTUGM")
+
+
+
+movies = [the_lure,
+          icaros_a_vision,
+          thelma,
+          city_of_ghosts,
+          alien_covenant,
+          super_dark_times,
+          atomic_blonde,
+          lady_bird,
+          lucky,
+          good_time,
+          the_killing_of_a_sacred_deer,
+          dawson_city,
+          the_blackcoats_daughter,
+          i_called_him_morgan,
+          the_lost_city_of_z,
+          phantom_thread,
+          i_dont_feel_at_home,
+          columbus,
+          marjorie_prime,
+          dunkirk,
+          lady_macbeth,
+          wormwood]
+
+fresh_tomatoes.open_movies_page(movies)
+          
+          
+
+
---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+
+    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
+        self.title = movie_title
+        self.movie_storyline = movie_storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
